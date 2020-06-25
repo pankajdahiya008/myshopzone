21d6983d051fa24300be7f7db54035ca01813fcc
PANKAJ DAHIYA <pankaj.id.008@gmail.com>
2020-06-25 19:43:32 +0530

    build express server and start point config

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'build','index.html')))
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
