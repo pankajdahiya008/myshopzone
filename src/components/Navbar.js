5ac8eaf0abdcbb290a73868022de45f0f8ac9bf1
PANKAJ DAHIYA <pankaj.id.008@gmail.com>
2020-07-01 17:36:17 +0530

    implemented Navbar using Bootsrap

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default class Navbar extends React.Component{
+    render() {
+        return (
+            <header className="App-header">
+                <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                    <a class="navbar-brand text-warning" href="/">myshopzone</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarCollapse">
+                        
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/signin">Sign In</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/cart">Cart</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/about">About</a>
+                            </li>
+                        </ul>
+                        <form class="form-inline mt-2 mt-md-0">
+                            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"></input>
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </div>
+                </nav>
+            </header>
+           
+        )
+    }
+}
