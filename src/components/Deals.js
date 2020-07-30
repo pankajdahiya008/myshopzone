47ab1c6f3a2ea28f2da882b8c171dcc46aa3377f
PANKAJ DAHIYA <pankaj.id.008@gmail.com>
2020-07-30 16:28:30 +0530

    made cards clickable and installed react-roter-dom

--- src/components/Deals.js
+++ src/components/Deals.js
@@ -7,21 +7,23 @@ export default class Slider extends React.Component {
                 <div className="row"><h4>Deals of the Day</h4>
                 </div>
                 <div className="row">
+                   
                 <div class="card m-2" style={{ width: 12 + 'rem' }}>
                     <img class="card-img-top" src="d1.jpg" alt="Unavailable"></img>
                         <div class="card-body">
                             <h6>Headphones</h6>
                             <p class="text-success">Upto 50% off</p>
-                        <p class="card-text text-secondary">JBL, Sony & more</p>
+                            <p class="card-text text-secondary">JBL, Sony & more</p>
+                            <a href="#" class="stretched-link"></a>
                     </div>
                 </div>
-                    
                     <div class="card m-2" style={{ width: 12 + 'rem' }}>
                         <img class="card-img-top" src="d2.jpg" alt="Unavailable"></img>
                         <div class="card-body">
                             <h6>Headphones</h6>
                             <p class="text-success">Upto 50% off</p>
                             <p class="card-text text-secondary">JBL, Sony & more</p>
+                            <a href="#" class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="card m-2" style={{ width: 12 + 'rem' }}>
@@ -30,6 +32,7 @@ export default class Slider extends React.Component {
                             <h6>Headphones</h6>
                             <p class="text-success">Upto 50% off</p>
                             <p class="card-text text-secondary">JBL, Sony & more</p>
+                            <a href="#" class="stretched-link"></a>
                         </div>
                     </div>
                     <div class="card m-2" style={{ width: 12 + 'rem' }}>
@@ -38,6 +41,7 @@ export default class Slider extends React.Component {
                             <h6>Headphones</h6>
                             <p class="text-success">Upto 50% off</p>
                             <p class="card-text text-secondary">JBL, Sony & more</p>
+                            <a href="#" class="stretched-link"></a>
                         </div>
                     </div>
                 </div>
