a39edf85ce46f1736f9ef784e055be3722ae2436
PANKAJ DAHIYA <pankaj.id.008@gmail.com>
2020-08-14 10:27:27 +0530

    app.get(*) in server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 3000
 const path = require('path')
 
 app.use(express.static(path.join(__dirname, 'build')))
-app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'build','index.html')))
+app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'build','index.html')))
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
