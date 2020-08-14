48d48ea298d12439633ec2a22bc4f9e677085334
PANKAJ DAHIYA <pankaj.id.008@gmail.com>
2020-08-14 10:59:32 +0530

    changed pic size at Deals page

--- src/components/Deals.js
+++ src/components/Deals.js
@@ -4,44 +4,48 @@ export default class Deals extends React.Component {
     render() {
         return (
             <div className="container-fluid">
-                <div className="row"><h4>Deals of the Day</h4>
-                </div>
+                <div className="row"><h4>Deals of the Day</h4></div>
+
                 <div className="row">
-                   
-                <div class="card m-2" style={{ width: 12 + 'rem' }}>
-                    <img class="card-img-top" src="d1.jpg" alt="Unavailable"></img>
+
+                    <div class="card col m-2">
+                        <img class="card-img-top img-thumbnail" src="d1.jpg" alt="Unavailable"></img>
                         <div class="card-body">
                             <h6>Shirt</h6>
                             <p class="text-success">Upto 50% off</p>
                             <p class="card-text text-secondary">JBL, Sony & more</p>
                             <a href="/Shirt" class="stretched-link"></a>
-                    </div>
-                </div>
-                    <div class="card m-2" style={{ width: 12 + 'rem' }}>
-                        <img class="card-img-top" src="d2.jpg" alt="Unavailable"></img>
-                        <div class="card-body">
-                            <h6>Headphones</h6>
-                            <p class="text-success">Upto 50% off</p>
-                            <p class="card-text text-secondary">JBL, Sony & more</p>
-                            <a href="#" class="stretched-link"></a>
+
                         </div>
                     </div>
-                    <div class="card m-2" style={{ width: 12 + 'rem' }}>
-                        <img class="card-img-top" src="d3.jpg" alt="Unavailable"></img>
+                    <div class="card col m-2">
+                        <img class="card-img-top img-thumbnail" src="d1.jpg" alt="Unavailable"></img>
                         <div class="card-body">
-                            <h6>Watch</h6>
+                            <h6>Shirt</h6>
                             <p class="text-success">Upto 50% off</p>
                             <p class="card-text text-secondary">JBL, Sony & more</p>
-                            <a href="#" class="stretched-link"></a>
+                            <a href="/Shirt" class="stretched-link"></a>
+
                         </div>
                     </div>
-                    <div class="card m-2" style={{ width: 12 + 'rem' }}>
-                        <img class="card-img-top" src="d4.jpg" alt="Unavailable"></img>
+                    <div class="card col m-2">
+                        <img class="card-img-top img-thumbnail" src="d1.jpg" alt="Unavailable"></img>
                         <div class="card-body">
-                            <h6>Fan</h6>
+                            <h6>Shirt</h6>
                             <p class="text-success">Upto 50% off</p>
                             <p class="card-text text-secondary">JBL, Sony & more</p>
-                            <a href="#" class="stretched-link"></a>
+                            <a href="/Shirt" class="stretched-link"></a>
+
+                        </div>
+                    </div>
+                    <div class="card col m-2">
+                        <img class="card-img-top img-thumbnail" src="d1.jpg" alt="Unavailable"></img>
+                        <div class="card-body">
+                            <h6>Shirt</h6>
+                            <p class="text-success">Upto 50% off</p>
+                            <p class="card-text text-secondary">JBL, Sony & more</p>
+                            <a href="/Shirt" class="stretched-link"></a>
+
                         </div>
                     </div>
                 </div>
@@ -52,4 +56,3 @@ export default class Deals extends React.Component {
 
 
 
-    
