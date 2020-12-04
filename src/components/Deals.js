9fbe6f6c23fc8d2ac52365f7d3169886848bb018
Pankaj Dahiya <pankaj.id.008@gmail.com>
2020-12-04 15:16:30 +0530

    changed landing page images

--- src/components/Deals.js
+++ src/components/Deals.js
@@ -13,36 +13,36 @@ export default class Deals extends React.Component {
                         <div class="card-body">
                             <h6>Shirt</h6>
                             <p class="text-success">Upto 50% off</p>
+                            <p class="card-text text-secondary">Raymond, COBB & more</p>
+                            <a href="/Shirt" class="stretched-link"></a>
+
+                        </div>
+                    </div>
+                    <div class="card col m-2">
+                        <img class="card-img-top img-thumbnail" src="d2.jpg" alt="Unavailable"></img>
+                        <div class="card-body">
+                            <h6>Earphone</h6>
+                            <p class="text-success">Upto 80% off</p>
                             <p class="card-text text-secondary">JBL, Sony & more</p>
                             <a href="/Shirt" class="stretched-link"></a>
 
                         </div>
                     </div>
                     <div class="card col m-2">
-                        <img class="card-img-top img-thumbnail" src="d1.jpg" alt="Unavailable"></img>
+                        <img class="card-img-top img-thumbnail" src="d3.jpg" alt="Unavailable"></img>
                         <div class="card-body">
-                            <h6>Shirt</h6>
-                            <p class="text-success">Upto 50% off</p>
-                            <p class="card-text text-secondary">JBL, Sony & more</p>
+                            <h6>Watch</h6>
+                            <p class="text-success">Upto 20% off</p>
+                            <p class="card-text text-secondary">Titan, Sonata & more</p>
                             <a href="/Shirt" class="stretched-link"></a>
 
                         </div>
                     </div>
                     <div class="card col m-2">
-                        <img class="card-img-top img-thumbnail" src="d1.jpg" alt="Unavailable"></img>
+                        <img class="card-img-top img-thumbnail" src="d4.jpg" alt="Unavailable"></img>
                         <div class="card-body">
-                            <h6>Shirt</h6>
-                            <p class="text-success">Upto 50% off</p>
-                            <p class="card-text text-secondary">JBL, Sony & more</p>
-                            <a href="/Shirt" class="stretched-link"></a>
-
-                        </div>
-                    </div>
-                    <div class="card col m-2">
-                        <img class="card-img-top img-thumbnail" src="d1.jpg" alt="Unavailable"></img>
-                        <div class="card-body">
-                            <h6>Shirt</h6>
-                            <p class="text-success">Upto 50% off</p>
+                            <h6>Fan</h6>
+                            <p class="text-success">Upto 10% off</p>
                             <p class="card-text text-secondary">JBL, Sony & more</p>
                             <a href="/Shirt" class="stretched-link"></a>
 
